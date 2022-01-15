d06a406def48fb5fafb69098e0e330540572f5cc
Sandy Vasquez <sandyvasquez9@gmail.com>
2022-01-15 13:48:20 -0800

    changing font size

--- myZooKeeper/AppDelegate.swift
+++ myZooKeeper/AppDelegate.swift
@@ -18,8 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       
       UITextView.appearance().font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle(rawValue: "Menlo"))
       
+      UITextField.appearance().font = .systemFont(ofSize: 17)
       UITextView.appearance().font = .systemFont(ofSize: 16)
-      UILabel.appearance().font = .boldSystemFont(ofSize: 16.0)
+      UILabel.appearance().font = .boldSystemFont(ofSize: 17.0)
 
       if #available(iOS 15.0, *) {
           let textAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
